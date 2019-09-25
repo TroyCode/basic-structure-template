faeb4958a529b1ff0a5a7ff013e2edfe5a36cdb0
Troy Wu <pightmp@gmail.com>
2019-09-25 23:42:21 +0800

    add create-react-app ejected and vue-cli-type-script

---
+++ create-react-app-3.1.2-ejected/build/precache-manifest.eff9cdfed66f7389ae7e05f88073515b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba0779b02897d6dff0a9fd7848303d06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f05d26576a5fdb065d",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "8b9eb3d17ac2597853b3",
+    "url": "/static/js/2.92f93f91.chunk.js"
+  },
+  {
+    "revision": "e9f05d26576a5fdb065d",
+    "url": "/static/js/main.7050f8d3.chunk.js"
+  },
+  {
+    "revision": "04af336b75a4adb69067",
+    "url": "/static/js/runtime-main.0c77dbe5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
